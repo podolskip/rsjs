bce767e2f1e20566a7e1a969054a576930cd45ea
Patryk Podolski <podolski.patryk@gmail.com>
2018-06-24 12:27:24 +0200

    processing mouse event

--- 3-reactive-programming-rxjs-getting-started-m3-exercise-files/before_TEST/main.ts
+++ 3-reactive-programming-rxjs-getting-started-m3-exercise-files/before_TEST/main.ts
@@ -1,74 +1,45 @@
-// import { Observer, Subject, ReplaySubject,  of, range } from 'rxjs';
 import { Observable } from 'rxjs/Observable';
 import { Observer } from 'rxjs/Observer';
 import { from } from 'rxjs/Observable/from';
+import { fromEvent } from 'rxjs/Observable/fromEvent';
 import  'rxjs/add/operator/map';
 import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/delay';
 
-
-// window.alert('ok!!!!');
-
-let nrArr = [1, 4, 5];
-
-// 1st option to implement observable
-let source = from(nrArr);
-let source2 = Observable.create(observer => {
-    
-    // for (let n of nrArr) {
-    //     observer.next(n);
-    // }
-
-    // observer.complete();
-
-    let index = 0;
-    let produceValur = () => {
-        observer.next(nrArr[index++]);
-
-        if ( index < nrArr.length ) {
-            setTimeout(produceValur, 2000);
-        } else {
-            observer.complete();
-        }
-
-    }
-
-    produceValur();
-}).map((n: any): number => {
-    return n * 2;
-})
-.filter((n: number): boolean => {
-    return n > 8;
-});
-
-
-
-
-class MyObserver implements Observer<number> {
-    next(value: number) {
-        console.log(`value ${value}`);
-    }
-
-    error(e: any) {
-        console.log(`we habe an error: ${e}`);
-    }
-
-    complete() {
-        console.log(`action completed`);
-    }
+interface IobjectXY {
+    x: number;
+    y: number;
 }
 
-// 1st option to implement observer
-// source.subscribe(new MyObserver());
+let circle: HTMLElement = document.getElementById('circle');
+let source2 = fromEvent( document, 'mousemove' )
+.map((e: MouseEvent) => {
+    return {
+        x: e.clientX,
+        y: e.clientY
+    };
+})
+.filter((value: IobjectXY) => {
+    return value.x < 500;
+})
+.delay(300);
 
-// // 2nd Option to implement observer
-// source.subscribe(
-//     value => console.log(`value ${value}`),
-//     e => console.log(`we habe an error: ${e}`),
-//     () => console.log(`action completed`)
+// .map((n: any): number => {
+//     return n * 2;
+// })
+// .filter((n: number): boolean => {
+//     return n > 8;
+// });
 
-// );
 source2.subscribe(
-    value => console.log(`value ${value}`),
+    (value: IobjectXY) => {
+        let circle: HTMLElement = document.getElementById('circle');
+
+        circle.style.left = String(value.x);
+        circle.style.top = String(value.y);
+
+        
+    },
     e => console.log(`we habe an error: ${e}`),
     () => console.log(`action completed`)
 
