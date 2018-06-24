71c4f549cac8eae459f690b47168819ea59aa2df
Patryk Podolski <podolski.patryk@gmail.com>
2018-06-24 15:16:11 +0200

    flatMap -> mergeMap

--- 3-reactive-programming-rxjs-getting-started-m3-exercise-files/before_TEST/main.ts
+++ 3-reactive-programming-rxjs-getting-started-m3-exercise-files/before_TEST/main.ts
@@ -5,38 +5,107 @@ import { fromEvent } from 'rxjs/Observable/fromEvent';
 import  'rxjs/add/operator/map';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/delay';
+import 'rxjs/add/operator/mergeMap';
+import { Subscriber } from 'rxjs';
 
 interface IobjectXY {
     x: number;
     y: number;
 }
 
-let circle: HTMLElement = document.getElementById('circle');
-let source2 = fromEvent( document, 'mousemove' )
-.map((e: MouseEvent) => {
-    return {
-        x: e.clientX,
-        y: e.clientY
-    };
-})
-.filter((value: IobjectXY) => {
-    return value.x < 500;
-})
-.delay(300);
+interface Imovie {
+    title: string;
+}
 
-// .map((n: any): number => {
-//     return n * 2;
+// let source2 = fromEvent( document.getElementById("getMoviesBtn"), 'click' )
+// .map((e: MouseEvent) => {
+//     console.log(e);
+//     // return {
+//     //     x: e.clientX,
+//     //     y: e.clientY
+//     // };
 // })
-// .filter((n: number): boolean => {
-//     return n > 8;
-// });
+// // .filter((value: IobjectXY) => {
+// //     return value.x < 500;
+// // })
+// // .delay(300);
 
-source2.subscribe(
-    (value: IobjectXY) => {
-        let circle: HTMLElement = document.getElementById('circle');
+// // .map((n: any): number => {
+// //     return n * 2;
+// // })
+// // .filter((n: number): boolean => {
+// //     return n > 8;
+// // });
 
-        circle.style.left = String(value.x);
-        circle.style.top = String(value.y);
+
+// let load = (url: string) => {
+//     let xhr: XMLHttpRequest = new XMLHttpRequest();
+
+//     xhr.addEventListener("load",() => {
+//         let movies: Array<Imovie> = JSON.parse(xhr.responseText);
+
+//         movies.forEach((movie: Imovie) => {
+//             let div: HTMLElement = document.createElement("div");
+//             div.innerText = movie.title;
+//             (document.getElementById("output") as HTMLElement).appendChild(div);
+//         });
+//     });
+
+//     xhr.open("GET", url);
+//     xhr.send();
+// }
+// source2.subscribe(
+//     (value) => {
+//         return load("movies.json");
+//         // let outputDiv: HTMLElement = document.getElementById('output');
+
+//         // circle.style.left = String(value.x);
+//         // circle.style.top = String(value.y);
+
+        
+//     },
+//     e => console.log(`we habe an error: ${e}`),
+//     () => console.log(`action completed`)
+
+// );
+
+
+let source2 = fromEvent( document.getElementById("getMoviesBtn"), 'click' );
+
+
+
+let load = (url: string) => {
+    return Observable.create(observer => {
+        let xhr: XMLHttpRequest = new XMLHttpRequest();
+    
+        xhr.addEventListener("load",() => {
+            let data: Array<Imovie> = JSON.parse(xhr.responseText);   
+            observer.next(data);
+        });
+    
+        xhr.open("GET", url);
+        xhr.send();
+
+    })
+}
+
+const renderMovies = (movies: Array<Imovie>) => {
+    movies.forEach((movie: Imovie) => {
+        let div: HTMLElement = document.createElement("div");
+        div.innerText = movie.title;
+        (document.getElementById("output") as HTMLElement).appendChild(div);
+    });
+}
+
+
+source2.mergeMap(e => load("movies.json"))
+.subscribe(
+    (movies: Array<Imovie>) => {
+        return renderMovies(movies);
+        // let outputDiv: HTMLElement = document.getElementById('output');
+
+        // circle.style.left = String(value.x);
+        // circle.style.top = String(value.y);
 
         
     },
